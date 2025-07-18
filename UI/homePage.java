fae59c725d70316c2a3a26c5d23378504a7bae31
haiqalhasly <haiqalhasly@gmail.com>
2025-07-18 15:27:19 +0800

    adding name text field

---
+++ UI/homePage.java
@@ -0,0 +1,50 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class homePage {
+    homePageFrame homePageFrame = new homePageFrame();
+    
+
+}
+
+class homePageFrame extends JFrame {
+    // Create ID field variable
+    private JTextField customerNameField;
+    private JComboBox<String> temperatureDropdown;
+    private JComboBox<String> PriorityType;
+
+    public homePageFrame() {
+
+
+        //Basic Login Layout
+        super("Home Page");
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
+
+        //customer Name field input
+
+        add(new JLabel("Enter your name:"));
+        customerNameField = new JTextField(10);
+        add(customerNameField);
+
+        // Temperature dropdown 
+
+        String[] choices = { "COLD", "MEDIUM", "HOT"};
+        temperatureDropdown = new JComboBox<>(choices);
+
+        //Priority Type
+
+        String[] priority = { "FIFO", "Queue" };
+        PriorityType = new JComboBox<>(priority);
+
+
+        JButton loginButton = new JButton("Proceed");
+        add(loginButton);
+
+        setVisible(true);
+    }
+}
--- main.java
+++ main.java
@@ -1,7 +1,16 @@
+import javax.swing.SwingUtilities;
+
+import UI.homePage;
+
 public class main {
     //run the program here
     public static void main(String[] args) {
-        
+
+                //Ensures GUI swings created and updated
+        SwingUtilities.invokeLater(() -> new homePage());
+        //Implements lambda expression () is the parameter. -> lambda operator ->  and new Login() is a method
+        //Makes the codes cleaner
+
         
     }
 }
